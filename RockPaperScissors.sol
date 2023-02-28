0bff7d846c594d3fdff6e3a348d7de409c3e2287
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-02-28 16:46:15 +0400

    add player

--- RockPaperScissors.sol
+++ RockPaperScissors.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.8.17;
 
 contract RockPaperScissors  {
-    string public choice;
+    string public choiceForFirstPlayer;
+    string public choiceForSecondPlayer;
+    address public firstPlayer;
+    address public secondPlayer;
 
-    function setChoice(string memory _choice) public {
-        require(keccak256(bytes(_choice)) == keccak256(bytes("rock")) || keccak256(bytes(_choice)) == keccak256(bytes("paper")) || keccak256(bytes(_choice)) == keccak256(bytes("scissors")), "Invalid choice");
-        choice = _choice;
+    function setChoiceForFirstPlayer(string memory _choiceForFirstPlayer) public {
+        require(msg.sender == firstPlayer, "you are not first player");
+        require(keccak256(bytes(_choiceForFirstPlayer)) == keccak256(bytes("rock")) || keccak256(bytes(_choiceForFirstPlayer)) == keccak256(bytes("paper")) || keccak256(bytes(_choiceForFirstPlayer)) == keccak256(bytes("scissors")), "Invalid choice");
+        choiceForFirstPlayer = choiceForFirstPlayer;
+    }
+    function setChoiceForSecondPlayer(string memory _choiceForSecondPlayer) public {
+        require(msg.sender == secondPlayer, "you are not second  player");
+        require(keccak256(bytes(_choiceForSecondPlayer)) == keccak256(bytes("rock")) || keccak256(bytes(_choiceForSecondPlayer)) == keccak256(bytes("paper")) || keccak256(bytes(_choiceForSecondPlayer)) == keccak256(bytes("scissors")), "Invalid choice");
+        choiceForSecondPlayer = _choiceForSecondPlayer;
     }
 }
